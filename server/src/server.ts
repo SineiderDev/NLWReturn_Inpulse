a7a43312e9a452044c5f1f1761a74b9fedba5335
SineiderDev <sineider@gmail.com>
2022-05-06 17:53:52 -0300

    correções...

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,12 @@
 import express from 'express';
 import { routes } from './routes';
 import cors from 'cors';
+import bodyParser from 'body-parser';
 
 const app = express();
 
 app.use(cors());
+app.use(bodyParser.json({limit:'900kb'}));
 app.use(express.json());
 app.use(routes);
 
